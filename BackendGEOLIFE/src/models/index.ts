63434299f579e26b8885c2eadcf50ad233749202
thunderthard <hars1302@gmail.com>
2022-11-07 14:50:39 -0500

    creacion modelo de servicio

--- BackendGEOLIFE/src/models/index.ts
+++ BackendGEOLIFE/src/models/index.ts
@@ -1,2 +1,3 @@
 export * from './persona.model';
 export * from './cliente.model';
+export * from './servicio.model';
---
+++ BackendGEOLIFE/src/models/servicio.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Servicio extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaDeSolicitud: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  estadoSolicitud: number;
+
+  @property({
+    type: 'string',
+  })
+  observaciones?: string;
+
+
+  constructor(data?: Partial<Servicio>) {
+    super(data);
+  }
+}
+
+export interface ServicioRelations {
+  // describe navigational properties here
+}
+
+export type ServicioWithRelations = Servicio & ServicioRelations;
