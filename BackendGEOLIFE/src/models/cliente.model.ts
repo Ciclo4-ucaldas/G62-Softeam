958666adeb3eee4be0ce47c760d5c5dca0ba42ef
thunderthard <hars1302@gmail.com>
2022-11-07 14:46:45 -0500

    creacion entidad cliente

---
+++ BackendGEOLIFE/src/models/cliente.model.ts
@@ -0,0 +1,35 @@
+import {model, property} from '@loopback/repository';
+import {Persona} from '.';
+
+@model()
+export class Cliente extends Persona {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
