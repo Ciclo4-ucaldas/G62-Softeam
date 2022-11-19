b8079606b0cc9234bb227161655b7a8b83e5f861
ingrid2294 <ingrid22esterilla@gmail.com>
2022-11-18 20:46:17 -0500

    creacion del metodo identificacion persona

--- BackendGEOLIFE/src/controllers/persona.controller.ts
+++ BackendGEOLIFE/src/controllers/persona.controller.ts
@@ -1,3 +1,4 @@
+import { service } from '@loopback/core';
 import {
   Count,
   CountSchema,
@@ -16,14 +17,44 @@ import {
   del,
   requestBody,
   response,
+  HttpErrors,
 } from '@loopback/rest';
 import {Persona} from '../models';
+import { Credencial } from '../models/credencial.model';
 import {PersonaRepository} from '../repositories';
+import { AutenticacionService } from '../services';
 
 export class PersonaController {
   constructor(
     @repository(PersonaRepository)
     public personaRepository : PersonaRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion:AutenticacionService
   ) {}
-  
+  @post("/identificarPersona",{
+    responses:{
+      '200':{
+        description:"identificar Usuario"
+      }
+    }
+  }
+  )
+  async identificarUsuario(@requestBody() credencial:Credencial){
+    let usuario=await this.servicioAutenticacion.identificarUsuario(credencial.usuario,credencial.contrasena)
+    let token;
+    if(usuario){
+       token =await this.servicioAutenticacion.generarToken(usuario,usuario.constructor.name);
+      return{
+        datos:{
+          nombres:usuario.Nombres,
+          correo:usuario.usuario,
+          id:usuario.id,
+          rol:usuario.constructor.name
+        },
+        tk:token
+      }
+    }else {
+      throw new HttpErrors[401]("datos invalidos")
+    }
+  }
 }
